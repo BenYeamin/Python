126580a8bea95b747ee19d4e9e7be740614ca78b
Ben Yeamin <30319065+BenYeamin@users.noreply.github.com>
2018-10-31 00:43:35 +0600

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,12 @@
+"""
+    @author: Ben Yeamin
+"""
+
+from math import sqrt
+
+sq = eval(input("Enter your number: "))
+
+print("The square root of your number is: ", sqrt(sq))
+
+
+input("Enter any key to exit")
